f368f46e54264f5487961d395d79ae060aab131a
Reza <reza.shahnazar@gmail.com>
2021-01-30 01:12:43 +0330

    Adding SimplerWeek and Venv

---
+++ simpleWeek.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+week = np.random.rand(7,3)
+weekFill = np.zeros((7,3), dtype=str)
+
+
+persons =['A','B','C','D']
+
+for i in range(0,7):
+    for j in range(0,3):
+        weekFill[i][j] = persons[round(week[i][j]*3)]
+
+print(weekFill)
