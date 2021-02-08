f06361351fa121d6a560305517d4e7e0a385d10c
Reza <reza.shahnazar@gmail.com>
2021-02-09 02:36:55 +0330

    Formatting GUI texts

--- GUI.py
+++ GUI.py
@@ -8,14 +8,24 @@ class Table:
         # code for creating table 
         for i in range(total_rows): 
             for j in range(total_columns): 
-                  
-                self.e = Entry(root, width=10, fg='white', 
-                               font=('Arial',16,'bold'), justify='center') 
+                if i ==0:
+                     self.e = Entry(root, width=10, fg='red', bg='white', bd=0,
+                                   font=('Arial',14,'bold'), 
+                                   justify='center')
+                else:
+                    if j==0:
+                        self.e = Entry(root, width=10, fg='black', bg='white', bd=0,
+                                    font=('Arial',14,'bold'), 
+                                    justify='center') 
+                    else: 
+                        self.e = Entry(root, width=10, fg='white', bd=0,
+                                    font=('Arial',14,'italic'), 
+                                    justify='center') 
                   
                 self.e.grid(row=i, column=j) 
                 self.e.insert(END, list[i][j])
 
-list= [['ER1', 'ER2', 'CCU']] + DoctorShifts.table
+list= [['CCU-ER1', 'CCU-ER2', 'OnCall']] + DoctorShifts.table
 for x in range(31):
     if x ==0 :
         list[x].insert(0,"Days")
